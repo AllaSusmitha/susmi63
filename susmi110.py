dbe7ea442b6362c3309820bc43ea3546036f7407
AllaSusmitha <41768199+AllaSusmitha@users.noreply.github.com>
2018-08-24 19:59:36 +0530

    Create susmi110.py

---
+++ susmi110.py
@@ -0,0 +1,13 @@
+num=int(raw_input())
+new=[]
+list=[int(x) for x in raw_input().split()]
+for i in range(0,num):
+	if(i==list[i]):
+		new.append(i)
+	elif((new==[]) and (i==(num-1))):
+		print('-1')
+s=sorted(new)
+k=len(s)
+for i in range(0,k):
+	print s[i],
+		
